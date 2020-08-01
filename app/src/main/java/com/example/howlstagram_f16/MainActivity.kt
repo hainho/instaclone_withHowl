fa1584f51b224b1631b1818a196aa8c55c53710c
Ha In Ho <hainho9704@gmail.com>
2020-08-01 16:58:07 +0900

    IC : add photo button implement, firebase storage integration
    A button for adding photos was implemented.
    Firebase's storage was linked to the app.
    When you click the add photo button, you request access to the photo library and access it when you receive access.
    When uploading photos, it was implemented to be stored in the Firebase storage.
    In the future, we will implement the ability for photos to be posted to the app.

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.howlstagram_f16
 
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.howlstagram_f16.navigation.AlarmFragment
-import com.example.howlstagram_f16.navigation.DetailViewFragment
-import com.example.howlstagram_f16.navigation.GridFragment
-import com.example.howlstagram_f16.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howlstagram_f16.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,7 +27,9 @@ class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo ->{
-
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm ->{
@@ -46,6 +50,7 @@ class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 
 
